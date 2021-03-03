8f7fb56ccb988a33e34184fe34f4c96f66995105
duran <1010506985@qq.com>
2021-03-03 09:40:25 +0800

    初始化

---
+++ src/main/java/com/study/jingqiduizhangkafka/JingqiduizhangKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.study.jingqiduizhangkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingqiduizhangKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingqiduizhangKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/jingqiduizhangkafka/JingqiduizhangKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jingqiduizhangkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingqiduizhangKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
